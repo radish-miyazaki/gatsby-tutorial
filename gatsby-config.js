373af269ec86b8610d4a8210ca477c05ffa6b7f0
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2021-04-23 18:55:18 +0900

    finish tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,24 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'Pandas Eating Lots',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    `gatsby-transformer-remark`,
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      }
+    }
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,78 @@
 import React from "react"
+import Layout from "../components/layout"
+import { graphql, Link } from "gatsby"
+import { css } from "@emotion/react"
+import { rhythm } from "../utils/typography"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <h1
+          css={css`
+            display: inline-block;
+            border-bottom: 1px solid;
+          `}
+        >
+          Amazing Pandas Eating Things
+        </h1>
+        <h4>{ data.allMarkdownRemark.totalCount } Posts</h4>
+        { data.allMarkdownRemark.edges.map(({ node }) => {
+          return (
+            <div key={ node.id }>
+              <Link
+                to={node.fields.slug}
+                css={css`
+                  text-decoration: none;
+                  color: inherit;
+                `}
+              >
+                <h3
+                  css={css`
+                    margin-bottom: ${rhythm(1 / 4)};
+                  `}
+                >
+                  { node.frontmatter.title }{" "}
+                  <span
+                    css={css`
+                      color: #bbb;
+                    `}
+                  >
+                    - { node.frontmatter.date }
+                  </span>
+                </h3>
+                <p>{ node.excerpt }</p>
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+    </Layout>
+  )
 }
+
+export default Home
+
+export const query = graphql`
+  query {
+      allMarkdownRemark(sort: {
+          fields: [frontmatter___date], order:DESC
+      })
+      {
+          edges {
+              node {
+                  frontmatter {
+                      title
+                      date
+                  }
+                  fields {
+                      slug
+                  }
+                  excerpt
+                  timeToRead
+                  html
+              }
+          }
+      }
+  }
+`
